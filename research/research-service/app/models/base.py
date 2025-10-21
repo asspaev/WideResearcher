5b63e2e1bfe422deb4105c311720e506eb4ea38e
Nikita Aspaev <asspaev@yandex.ru>
2025-10-21 12:15:43 +0500

    feature(research-service): create declarative base

---
+++ research/research-service/app/models/base.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Integer
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
+
+from app.utils import camel_case_to_snake_case
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{camel_case_to_snake_case(cls.__name__)}s"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
