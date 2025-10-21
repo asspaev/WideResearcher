de36c1c5bf49d82028ead7a4168f3a22b7e0682b
Nikita Aspaev <asspaev@yandex.ru>
2025-10-21 11:54:35 +0500

    feature(research-service): init lifespan with database gateway dispose and add him to fastapi app

--- research/research-service/app/main.py
+++ research/research-service/app/main.py
@@ -1,10 +1,26 @@
-import uvicorn
-from app.routers import router as api_router
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+import uvicorn
+from fastapi import FastAPI
+from loguru import logger
+
+from app.models import get_db_gateway
+from app.routers import router as api_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    logger.info("Starting up database gateway...")
+    yield
+    # shutdown
+    logger.info("Shutting down database gateway...")
+    await get_db_gateway().dispose()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(api_router)
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True, log_level="info")
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8100, reload=True, log_level="info")
