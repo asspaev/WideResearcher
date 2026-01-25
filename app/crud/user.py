19e693d5e6c675e8c12b00638db2feb576eaf8b6
Nikita Aspaev <asspaev@yandex.ru>
2026-01-25 17:16:29 +0500

    feature(crud): init user crud, add check_user_exists_by_login and create_user funcs

---
+++ app/crud/user.py
@@ -0,0 +1,33 @@
+from sqlalchemy import exists, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models import User
+
+
+async def check_user_exists_by_login(
+    session: AsyncSession,
+    login: str,
+) -> bool:
+    """Проверяет существует ли пользователь с таким логином в базе данных"""
+    stmt = select(exists().where(User.user_login == login))
+
+    result = await session.execute(stmt)
+    return result.scalar()
+
+
+async def create_user(
+    session: AsyncSession,
+    login: str,
+    hashed_password: bytes,
+) -> User:
+    """Создает пользователя в базе данных"""
+    user = User(
+        user_login=login,
+        user_password_hash=hashed_password,
+    )
+
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+
+    return user
