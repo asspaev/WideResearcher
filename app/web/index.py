d36cccc9132a2da18a9d318529cf9bc866d59d8a
Nikita Aspaev <asspaev@yandex.ru>
2026-01-26 14:55:15 +0500

    refactor(web): refactor index page

--- app/web/index.py
+++ app/web/index.py
@@ -1,13 +1,11 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import RedirectResponse
 from loguru import logger
-
-from app.core.templates import templates
 
 router = APIRouter()
 
 
-@router.get("/", name="index", response_class=HTMLResponse)
+@router.get("/", name="index", response_class=RedirectResponse)
 async def index(request: Request):
     try:
         return RedirectResponse(url="/main")
