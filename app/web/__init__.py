9c7cd5fd7157261f69aef95bbbe656bce35c454e
Nikita Aspaev <asspaev@yandex.ru>
2026-01-28 14:15:53 +0500

    update(web): rename main to index

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -2,12 +2,9 @@ from fastapi import APIRouter
 
 from .auth import router as router_auth
 from .index import router as router_index
-from .main import router as router_main
 
 router = APIRouter(tags=["web"])
 
 router.include_router(router_index)
 
 router.include_router(router_auth)
-
-router.include_router(router_main)
--- app/web/index.py
+++ app/web/index.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import RedirectResponse
-from loguru import logger
+
+from app.core.templates import templates
 
 router = APIRouter()
 
 
 @router.get("/", name="index", response_class=RedirectResponse)
 async def index(request: Request):
-    try:
-        return RedirectResponse(url="/main")
-    except Exception as e:
-        logger.error(f"Error rendering index page: {e}")
+    return templates.TemplateResponse(
+        "pages/index.html",
+        {"request": request},
+    )
