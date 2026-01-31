496eff516f5bebf077346e44779d7114995f6fce
Nikita Aspaev <asspaev@yandex.ru>
2026-01-31 13:50:50 +0500

    feature(web): add new popup new_model

--- app/web/popup.py
+++ app/web/popup.py
@@ -6,7 +6,7 @@ from app.core.templates import templates
 router = APIRouter(prefix=get_settings().prefix.popup, tags=["popup"])
 
 
-@router.get("/new_research", name="new_research")
+@router.get("/new-research", name="new_research")
 async def get_popup_new_research(request: Request):
     """Рендер всплывающего окна для создания исследования"""
     return templates.TemplateResponse(
@@ -15,8 +15,17 @@ async def get_popup_new_research(request: Request):
     )
 
 
+@router.get("/new-model", name="new_model")
+async def get_popup_new_model(request: Request):
+    """Рендер всплывающего окна для создания модели"""
+    return templates.TemplateResponse(
+        "includes/popups/new_model.html",
+        {"request": request},
+    )
+
+
 @router.get("/hide", name="hide")
-async def get_popup_new_research(request: Request):
+async def get_popup_hide(request: Request):
     """Рендер закрытое всплывающее окно"""
     return templates.TemplateResponse(
         "includes/hidden_popup_overlay.html",
