b3e8cbb2501b84223a7f8e2099b5c7f19741b5d8
Nikita Aspaev <asspaev@yandex.ru>
2026-02-01 15:23:36 +0500

    refactor(services): move logic get researches to data_fetch for miltiple use code

--- app/services/data_fetch.py
+++ app/services/data_fetch.py
@@ -1,19 +1,26 @@
+from datetime import datetime, timezone
+
 from fastapi import HTTPException
+from fastapi.exceptions import HTTPException
 from loguru import logger
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.model import get_models_by_user_id
 from app.crud.model_output import count_model_outputs_by_model_id
-from app.models import Model
+from app.crud.research import get_all_researches_with_schedules_by_user_id
+from app.models import Model, Research, ResearchSchedule
 from app.schemas.model import ModelCard
+from app.schemas.research import ResearchCard
 from app.schemas.user import UserCookie
-from app.utils.datetime import format_added_at
+from app.utils.datetime import format_added_at, human_delta
 
 
 async def get_models_cards(
     user_cookie: UserCookie,
     session: AsyncSession,
 ) -> list[ModelCard]:
+    """Получение списка моделей пользователя"""
+
     # Получение моделей пользователя
     try:
         models: list[Model] = await get_models_by_user_id(session, user_cookie.user_id)
@@ -45,3 +52,39 @@ async def get_models_cards(
 
     # Возврат списка моделей
     return models_cards
+
+
+async def get_researches_cards(
+    user_cookie: UserCookie,
+    session: AsyncSession,
+) -> list[dict]:
+    """Получение списка исследований пользователя"""
+
+    # Получение всех исследования и планов исследований пользователя
+    try:
+        researches_with_schedules: tuple[Research, ResearchSchedule | None] = (
+            await get_all_researches_with_schedules_by_user_id(session, user_cookie.user_id)
+        )
+    except Exception as e:
+        logger.error(f"Error getting researches: {e}")
+        raise HTTPException(status_code=500, detail="Error getting researches")
+
+    # Подготовка списка исследований researches
+    researches: list[dict] = []
+    for research, schedule in researches_with_schedules:
+        research_last_update_time: str = human_delta(research.meta_updated_at, datetime.now(timezone.utc))
+        if schedule is None:
+            schedule_next_launch_time: str = "не запланировано"
+        else:
+            schedule_next_launch_time: str = human_delta(schedule.scheduled_at, datetime.now(timezone.utc))
+        research_card: ResearchCard = ResearchCard(
+            research_id=research.research_id,
+            research_name=research.research_name,
+            research_version_name=research.research_version_name,
+            research_last_update_time=research_last_update_time,
+            schedule_next_launch_time=schedule_next_launch_time,
+        )
+        researches.append(research_card)
+
+    # Возврат списка исследований
+    return researches
--- app/web/index.py
+++ app/web/index.py
@@ -7,10 +7,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.sql import get_session
 from app.core.templates import templates
-from app.crud.research import get_all_researches_with_schedules_by_user_id, get_next_planned_research_by_user_id
+from app.crud.research import get_next_planned_research_by_user_id
 from app.models import Research, ResearchSchedule
-from app.schemas.research import NearestResearch, ResearchCard
+from app.schemas.research import NearestResearch
 from app.schemas.user import UserCookie
+from app.services.data_fetch import get_researches_cards
 from app.utils.datetime import human_delta
 from app.utils.dependencies import get_user_cookie
 
@@ -24,31 +25,9 @@ async def get_index(
     session: AsyncSession = Depends(get_session),
 ):
     """Рендер главной страницы"""
-    # Получение всех исследования и планов исследований пользователя
-    try:
-        researches_with_schedules: tuple[Research, ResearchSchedule | None] = (
-            await get_all_researches_with_schedules_by_user_id(session, user_cookie.user_id)
-        )
-    except Exception as e:
-        logger.error(f"Error getting researches: {e}")
-        raise HTTPException(status_code=500, detail="Error getting researches")
 
-    # Подготовка списка исследований researches
-    researches: list[dict] = []
-    for research, schedule in researches_with_schedules:
-        research_last_update_time: str = human_delta(research.meta_updated_at, datetime.now(timezone.utc))
-        if schedule is None:
-            schedule_next_launch_time: str = "не запланировано"
-        else:
-            schedule_next_launch_time: str = human_delta(schedule.scheduled_at, datetime.now(timezone.utc))
-        research_card: ResearchCard = ResearchCard(
-            research_id=research.research_id,
-            research_name=research.research_name,
-            research_version_name=research.research_version_name,
-            research_last_update_time=research_last_update_time,
-            schedule_next_launch_time=schedule_next_launch_time,
-        )
-        researches.append(research_card)
+    # Получение исследований пользователя
+    researches: list[dict] = await get_researches_cards(user_cookie, session)
 
     # Получение ближайшего исследования пользователя
     try:
