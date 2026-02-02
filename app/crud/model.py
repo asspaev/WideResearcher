6288e73c8f0fa5df6584e7af70bf2401920eb4e8
Nikita Aspaev <asspaev@yandex.ru>
2026-02-02 20:25:10 +0500

    feature(crud): add crud funcs get_model_by_id and update_model

--- app/crud/model.py
+++ app/crud/model.py
@@ -64,3 +64,56 @@ async def create_model(
     await session.commit()
     await session.refresh(new_model)
     return new_model
+
+
+async def get_model_by_id(
+    session: AsyncSession,
+    model_id: int,
+) -> Model | None:
+    """
+    Возвращает модель по model_id.
+    """
+    stmt = select(Model).where(Model.model_id == model_id)
+
+    result = await session.execute(stmt)
+    return result.scalar_one_or_none()
+
+
+async def update_model(
+    session: AsyncSession,
+    model_id: int,
+    *,
+    model_type: str | None = None,
+    model_name: str | None = None,
+    model_api_type: str | None = None,
+    model_path: str | None = None,
+    model_key_api: str | None = None,
+    model_key_answer: str | None = None,
+) -> Model | None:
+    """
+    Обновляет модель по model_id.
+    Возвращает обновлённый объект Model или None, если модель не найдена.
+    """
+
+    result = await session.execute(select(Model).where(Model.model_id == model_id))
+    model: Model | None = result.scalar_one_or_none()
+
+    if model is None:
+        return None
+
+    if model_type is not None:
+        model.model_type = model_type
+    if model_name is not None:
+        model.model_name = model_name
+    if model_api_type is not None:
+        model.model_api_type = model_api_type
+    if model_path is not None:
+        model.model_path = model_path
+    if model_key_api is not None:
+        model.model_key_api = model_key_api
+    if model_key_answer is not None:
+        model.model_key_answer = model_key_answer
+
+    await session.commit()
+    await session.refresh(model)
+    return model
