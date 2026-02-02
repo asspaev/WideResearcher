db7041a48e670e6087aa0a69bede18c0d6046cf1
Nikita Aspaev <asspaev@yandex.ru>
2026-02-02 20:25:28 +0500

    feature(web): add web endpoint GET /edit-model/{model_id}

--- app/web/popups.py
+++ app/web/popups.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, Request
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.config import get_settings
+from app.core.sql import get_session
 from app.core.templates import templates
+from app.crud.model import get_model_by_id
+from app.models import Model
 
 router = APIRouter(prefix=get_settings().prefix.popups, tags=["popups"])
 
@@ -30,4 +34,28 @@ async def get_popup_hide(request: Request):
     return templates.TemplateResponse(
         "includes/hidden_popup_overlay.html",
         {"request": request},
+    )
+
+
+@router.get("/edit-model/{model_id}", name="edit_model")
+async def get_edit_model(
+    request: Request,
+    model_id: int,
+    session: AsyncSession = Depends(get_session),
+):
+    """Рендер всплывающего окна редактирования модели"""
+    # TODO Проверка, что модель принадлежит пользователю
+
+    # Поиск модели
+    model: Model | None = await get_model_by_id(session, model_id)
+
+    # TODO Обработка при не удачном поиске
+
+    # Рендер всплывающего онка
+    return templates.TemplateResponse(
+        "includes/popups/edit_model.html",
+        {
+            "request": request,
+            "model": model,
+        },
     )
